a29b3ab5086be2dc99dc00760a2ee3543d1b4ca9
Rafael Fuentes Cantero <73295113+RafaFuentes4@users.noreply.github.com>
2026-05-15 19:32:17 +0200

    Add: eating-bananas - submission-10

---
+++ Data Structures & Algorithms/eating-bananas/submission-10.py
@@ -0,0 +1,29 @@
+import math
+
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        left, right = 1, max(piles)
+
+        min_speed = right
+
+        def can_eat(k):
+            speed = 0
+
+            for number_bananas in piles:
+                speed += math.ceil(float(number_bananas) / k)
+
+            return speed <= h
+
+        while left <= right:
+            k = (left + right) // 2
+
+            if can_eat(k):
+                right = k - 1
+                min_speed = k
+
+            else:
+                left = k + 1
+
+        return min_speed
+
+        
